e48b4f9d6ed96877c056068ee5ca92b131e2b969
rohithpeddi7 <peddi.rohith.cse21@itbhu.ac.in>
2023-05-19 14:03:25 +0530

    Fixed cd command

--- Shell/rpshell.c
+++ Shell/rpshell.c
@@ -10,7 +10,7 @@
 #define COLOR_BOLD  "\e[1m"
 #define COLOR_OFF   "\e[m"
 
-int concurrency=0;
+int concurrency=0, change_dir[2];// change_dir is a pipe for sending new_dir
 
 char cmd[MAXLEN],*args[MAXARGS],*wel[] = {"cat","rpshlogo.txt",NULL};
 
@@ -128,6 +128,13 @@ void execute_cmd()
         else if (*status==2);
         else if (*status!=0)
             printf("Invalid Command! Try again.\n");
+        char new_dir[1024];
+        if (read(change_dir[0],new_dir,1024)>0)
+        {
+            printf("new dir : %s\n",new_dir);
+            if (chdir(new_dir)<0)
+                printf("No such directory: %s\n",new_dir);
+        }
         return;
     }
     signal(SIGINT,SIG_DFL);
@@ -153,7 +160,10 @@ void execute_cmd()
             else if (i==1)
                 print_history();
             else if (i==2)
-                chdir(args[1]);
+            {
+                int y = write(change_dir[1],args[1],strlen(args[1])+1);
+                if (y<0) printf("Change directory failed due to OS error!\n");
+            }
             else if (i==3)
             {
                 printf("\nThanks for using Rohith Peddi Shell (RPSH)\nGood Bye\n");
@@ -161,9 +171,13 @@ void execute_cmd()
             }
             else if (i==4)
                 printf("\e[1;1H\e[2J");
+            close(change_dir[0]);
+            close(change_dir[1]);
             exit(0);
         }
     }
+    close(change_dir[0]);
+    close(change_dir[1]);
     if (execvp(args[0],args)<0)
         exit(-1);
     exit(0);
@@ -181,7 +195,9 @@ int main()
 {
     signal(SIGINT,signal_handler);
     signal(SIGTSTP,signal_handler);
+    int y =pipe(change_dir);
     welcome();
+    if (y<0) printf("The pipe communication has failed! cd command will not work!");
     while(7)
     {
         print_prompt();
@@ -191,3 +207,4 @@ int main()
     }
     return 0;
 }
+
